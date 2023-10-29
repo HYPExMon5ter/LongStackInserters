80c99b2937e58aff19fba13b03e7752940ff3e46
HYPExMon5ter <brackettblake@gmail.com>
2023-10-28 21:26:49 -0400

    Add project files.

---
+++ LongStackInsertersPlugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using StackLongInserters.Patches;
+
+namespace LongStackInserters
+{
+    // Token: 0x02000002 RID: 2
+    [BepInPlugin("net.hypexmon5ter.LongStackInserters", "LongStackInserters", "1.0.0")]
+    public class LongStackInsertersPlugin : BaseUnityPlugin
+    {
+        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+        private void Awake()
+        {
+            base.Logger.LogInfo("PluginName: LongStackInserters, VersionString: 1.0.0 is loading...");
+            LongStackInsertersPlugin.Harmony.PatchAll();
+            base.Logger.LogInfo("PluginName: LongStackInserters, VersionString: 1.0.0 is loaded.");
+            LongStackInsertersPlugin.Log = base.Logger;
+            Harmony.CreateAndPatchAll(typeof(InserterDefinitionPatch), null);
+        }
+
+        // Token: 0x04000001 RID: 1
+        private const string MyGUID = "net.hypexmon5ter.LongStackInserters";
+
+        // Token: 0x04000002 RID: 2
+        private const string PluginName = "LongStackInserters";
+
+        // Token: 0x04000003 RID: 3
+        private const string VersionString = "1.0.0";
+
+        // Token: 0x04000004 RID: 4
+        private static readonly Harmony Harmony = new Harmony("net.hypexmon5ter.LongStackInserters");
+
+        // Token: 0x04000005 RID: 5
+        public static ManualLogSource Log = new ManualLogSource("LongStackInserters");
+    }
+}
---
+++ Patches/InserterDefinitionPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace StackLongInserters.Patches
+{
+    // Token: 0x02000003 RID: 3
+    public class InserterDefinitionPatch
+    {
+        // Token: 0x06000004 RID: 4 RVA: 0x000020D0 File Offset: 0x000002D0
+        [HarmonyPatch(typeof(InserterDefinition), "InitInstance")]
+        [HarmonyPostfix]
+        private static void setToStack(ref InserterInstance newInstance)
+        {
+            if (newInstance.armLength >= 2)
+            {
+                newInstance.isStack = true;
+            }
+        }
+
+        // Token: 0x04000006 RID: 6
+        private static bool stackInserterUnlocked;
+    }
+}
